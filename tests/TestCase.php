bb0181383fb9831acdc5afa68c33ef17690acfbb
flexterful <shulzas@gmail.com>
2024-10-22 16:50:28 +0300

    style(CA-3): Update code and configuration to match code test styling rules

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,10 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/** @property Application $app */
 abstract class TestCase extends BaseTestCase
 {
     //
